b85bb1a4b7a4eb5f6c3ad0ddccbc8b1fd2b4b0a5
Tanisha Deshmukh <Tanisha-Deshmukh@users.noreply.github.com>
2026-03-31 21:28:09 +0530

    fix: typo in reconciliation db connection host property

--- upi-recon-system/upi-reconciliation/src/db/index.js
+++ upi-recon-system/upi-reconciliation/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_name } from "../constants.js";
 export const connectDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
-        console.log(`\n Mongodb connected !! DB HOST: ${connectionInstance.connect.host}`)
+        console.log(`\n Mongodb connected !! DB HOST: ${connectionInstance.connection.host}`)
    } catch (error) {
         console.log("MONGODB CONNECTION ERRPR", error);
         process.exit(1)
