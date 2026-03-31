d8e01b79074cc7c7a8e2279846a04ad3b836d28d
Tanisha Deshmukh <Tanisha-Deshmukh@users.noreply.github.com>
2026-03-31 19:48:11 +0530

    chore: optimized project structure and ignored logs

---
+++ start_all.bat
@@ -0,0 +1,4 @@
+start "React Frontend" cmd /k "cd upi-recon-system\react-app && npm run dev"
+start "UPI Payment API" cmd /k "cd upi-recon-system\upiPaymentSystem && npm run dev"
+start "UPI Reconciliation API" cmd /k "cd upi-recon-system\upi-reconciliation && npm run dev"
+start "Mock Bank API" cmd /k "cd upi-recon-system\mock-bank-api && npm run dev"
